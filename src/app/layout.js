b58b177c218f8648a0289467092c3aa113a586b2
supayangit <supayanchk@gmail.com>
2026-06-07 21:50:28 +0600

    navbar polished,layout styled

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
       data-theme="dark"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col gap-10">
+      <body className="min-h-full flex flex-col">
         <Navbar></Navbar>
         <main>
           {children}
